981bf9e3a825494de42c57d96d67a5d151ffe18b
Timur <zamilov05@mail.ru>
2024-03-07 23:49:55 +0300

    Добавьте файлы проекта.

---
+++ WinFormsXMLJSON/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+
+namespace WinFormsXMLJSON
+{
+    internal static class Program
+    {
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            var xmlDoc = new XDocument(new XDeclaration("1.0", "utf-8", "yes"),
+                new XElement("Transport"));
+
+            foreach (var item in Transport.GetTransports())
+            {
+                xmlDoc.Root.Add(new XElement("Transport", new XAttribute("Name", item.Name)),
+                    new XElement("Transport", new XAttribute("route", item.route)),
+                    new XElement("Transport", new XAttribute("Capacity", item.Capacity)),
+                    new XElement("Transport", new XAttribute("Speed", item.Speed)),
+                    new XElement("Transport", new XAttribute("Cost", item.Cost)));
+
+            }
+            xmlDoc.Save(Path.Combine(Environment.CurrentDirectory, "xmlDoc.xml"));
+
+            List<Transport> transportList = new List<Transport>();
+
+            foreach (var item in Transport.GetTransports())
+            {
+                transportList.Add(item);
+            }
+            string jsonData = JsonConvert.SerializeObject(transportList);
+
+            File.WriteAllText("Transport.json", jsonData);
+
+            Console.Read();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /*public partial class MainForm : Form
+        {
+            public MainForm()
+            {
+                InitializeComponent();
+                LoadData();
+            }
+
+            private void LoadData() 
+            {
+
+            }
+        }
+            */
---
+++ WinFormsXMLJSON/Transport.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsXMLJSON
+{
+    internal class Transport
+    {
+    public string Name { get; set; }
+    public Route route { get; set; }
+    public string Capacity { get; set; }
+    public string Speed { get; set; }
+    public string Cost { get; set; }
+
+        public static IEnumerable<Transport> GetTransports()
+        {
+            return new List<Transport>
+            {
+                new Transport{ Name ="Bus",route = new Route {Start = "Kazan", End = "Ufa"},Capacity = "65",Speed =  "100",Cost = "100" },
+                new Transport{ Name ="Trolleybus",route = new Route {Start = "Kremlin", End = "Gorki"},Capacity = "55",Speed =  "70",Cost = "90" },
+                new Transport{Name = "Tram",route = new Route {Start = "Sukonnaya Sloboda", End = "Tukai Square"},Capacity = "40",Speed = "55",Cost = "60" }
+            };
+        }
+    }
+
+    internal class Route
+    {
+        public string Start { get; set; }
+        public string End { get; set; }
+    }
+    internal class Conductor
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int WorkAge { get; set; }
+    }
+
+}
+
